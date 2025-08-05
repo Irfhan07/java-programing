06f07d94227cf713c0a73f73f1a2886706a40906
Irfhan07 <abdulirfhan9478@mountzion.ac.in>
2025-08-05 12:26:49 +0530

    for loop.java

---
+++ for loop.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+class main {
+
+    public static void main(String[] args) {
+
+        Scanner data = new Scanner(System.in);
+        
+        int A;
+        System .out.println("Enter your number:");
+        A=data.nextInt();
+        
+        for(A=0;A<=10;A++){
+            
+            if (A==6){
+                
+                continue;
+                
+            }
+            
+            else if(A==8){
+                
+                break;
+                
+            }
+            
+        System.out.println(A);
+        }
+        
+       
+            
+            
+            
+            
+            
+            
+            
+    }
+    }
+    
+    
+
+            
+            
