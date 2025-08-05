ae01039f81052d0dc5821baeafca21cff004a366
Irfhan07 <abdulirfhan9478@mountzion.ac.in>
2025-08-05 10:04:35 +0530

    ex.1.java

---
+++ ex.1.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+class main {
+
+    public static void main(String[] args) {
+
+        Scanner data = new Scanner(System.in);
+        
+        int day;
+        System .out.print("Enter your number:");
+        day=data.nextInt();
+        
+        switch(day){
+            
+            case 1:
+                
+                System.out.print("sunday");
+                break;
+                
+            case 2:
+                
+                System.out.print("monday");
+                break;
+                
+            case  3:
+                
+                System.out.print("thu");
+                break;
+                
+            case 4:
+            
+                
+                System.out.print("wen");
+                break;
+                
+                
+            
+            
+            
+        }
+    }
+}
+
+
+Syntax:
+
+Switch(ex):
+     
+       case 1:
+              
+            code
+            break;
+       case 2:
+            code
+            break;
+        case 3:
+            code
+            break;
+
+    ex……,
+
+            
+            
+        
+        
