deae94a482c10302cb00331d345dc08f1e32ded4
Irfhan07 <abdulirfhan9478@mountzion.ac.in>
2025-08-08 12:22:56 +0530

    project1.java

--- project1.java
+++ project1.java
@@ -2,36 +2,33 @@ import java.util.Scanner;
 
 class Main {
      
-           static int Add(int A,int B){
+           static double Add(double A,double B){
                 
                 return A+B;
                 }
-        
-       
-           static int Sub(int A,int B){
+           static double Sub(double A,double B){
                
                return A-B;
-       }
-        
-       
-           static int Mult(int A,int B){
+           }
+           static double Mult(double A,double B){
                
                return A*B;
        }
-       
-       
         public static void main(String[] args) {
-          
+            
+            System.out.println("1.add");
+            System.out.println("2.sub");
+            System.out.println("3.Mult");
            Scanner data = new Scanner(System.in);
         System.out.println("Enter the number ");
         int n = data.nextInt();
         
         
-        System.out.println("Enter the number1 ");
-        int A= data.nextInt();
+        System.out.println("Enter the num1 ");
+       double A= data.nextInt();
         
-        System.out.println("Enter your number2");
-        int B=data.nextInt();
+        System.out.println("Enter your num2");
+        double B=data.nextInt();
         
         
 
@@ -39,18 +36,18 @@ class Main {
           
             case 1:
                 
-               A=Add(A,B);
-               
-               System.out.println(A);
-               
-               
+             double r=Add(A,B);
+              System.out.println(r);
+              break;
+                
             case 2:
+               
                System.out.println(Sub(A,B));
-              
+               break;
             case 3:
                 
-               System.out.println( Mult(A,B));
-              
+               System.out.println(Mult(A,B));
+              break;
           }  
             
          
